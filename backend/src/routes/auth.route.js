a11f3cbbba286d30f056aba8c35f6eb675aed622
Ronak Prajapati <shaileshkumarprajapati11200>
2026-03-07 22:37:03 +0530

    Fix CORS issue and deployment bugs

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { logout, onboard } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/onboarding", protectRoute, onboard);
